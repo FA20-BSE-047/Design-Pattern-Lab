0f73b6b3faa244dd34f44284c1d2967a9f29d036
FA20-BSE-047 <102282639+FA20-BSE-047@users.noreply.github.com>
2023-09-23 16:06:24 -0700

    update
    complete the lab to part 1 and part 2 work

---
+++ LAb 2/LAB 2 part 1/src/pk/cuiatd/database/Demo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pk.cuiatd.database;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Connection c = Connection.getInstance();
+		c.getData();
+
+	}
+
+}
---
+++ LAb 2/LAB 2 part 1/src/pk/edu/cuiatd/configuration/Demo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pk.edu.cuiatd.configuration;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Configuration config = Configuration.getInstance();
+		System.out.println(config.getValue("mode"));
+		System.out.println(config.getValue("font-size"));
+		System.out.println(config.getValue("font-type"));
+
+	}
+
+}
