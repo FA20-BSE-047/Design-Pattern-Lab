36046d2da79d0cb1d59822bd8e29c0fc3dcb251f
FA20-BSE-047 <102282639+FA20-BSE-047@users.noreply.github.com>
2023-12-09 23:30:14 -0800

    state pattern work updated

---
+++ DP/src/gof/State/State.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gof.State;
+
+/**
+ *
+ * @author elitebook g3
+ */
+public class State {
+    
+}
